cb77aa0b955bf1f76e49ec1fd41fe5a7e25f1b45
pranjxl <pranjalpratap2580@gmail.com>
2026-05-03 18:54:32 +0530

    final: complete inference pipeline with source_age, preserve_id, delta_max, no_align flags

--- resize_256_small.py
+++ resize_256_small.py
@@ -2,8 +2,8 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
-SRC_DIR = "datasets/faces"
-DST_DIR = "datasets/faces_256"
+SRC_DIR = "datasets/faces_256_small"
+DST_DIR = "datasets/faces_256_small_resized"
 
 os.makedirs(DST_DIR, exist_ok=True)
 
@@ -11,4 +11,4 @@ for fname in tqdm(os.listdir(SRC_DIR)):
     if fname.lower().endswith((".jpg", ".png", ".jpeg")):
         img = Image.open(os.path.join(SRC_DIR, fname)).convert("RGB")
         img = img.resize((256, 256), Image.BILINEAR)
-        img.save(os.path.join(DST_DIR, fname))
+        img.save(os.path.join(DST_DIR, fname), quality=95)
